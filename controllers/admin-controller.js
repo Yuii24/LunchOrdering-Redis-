46a2205357a854181f4b45f43481ff09c1ccd7f1
Yuii24 <linian2040@gmail.com>
2024-08-20 05:50:21 +0800

    add meals and restaurants, create and edit

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -44,7 +44,6 @@ const adminController = {
       nest: true
     })
       .then(restaurants => {
-        console.log(restaurants)
         res.render('admin/restaurants', { restaurants })
       })
       .catch(err => next(err))
@@ -54,10 +53,6 @@ const adminController = {
   },
   psotRestaurants: async (req, res, next) => {
     const { name, tel, address, description } = req.body
-    console.log('name', name)
-    console.log('tel', tel)
-    console.log('adcress', address)
-    console.log('description', description)
 
     const descriptionArray = description.split('\n').map(d => {
       const [productName, price] = d.split(',').map(item => item.trim())
@@ -93,6 +88,92 @@ const adminController = {
 
       res.redirect('/admin/restaurants')
       // res.status(201).json({ message: 'Restaurant and menu items created successfully', restaurant });
+    } catch (err) {
+      // 出现错误时回滚事务
+      if (transaction) await transaction.rollback();
+      next(err);
+    }
+  },
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      include: [Meal]
+    })
+      .then(restaurant => {
+        res.render('admin/restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  editRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      include: [Meal],
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("此餐廳不存在")
+
+        res.render('admin/edit-restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  putRestaurant: async (req, res, next) => {
+    const restId = req.params.id
+    const { name, tel, address, description } = req.body
+    if (!name) throw new Error("請輸入餐廳名稱")
+    if (!tel) throw new Error("請輸入餐廳電話")
+
+    const descriptionArray = description.split('\n').map(d => {
+      const [productName, price] = d.split(',').map(item => item.trim());
+      return { productName, price: parseFloat(price) };
+    }).filter(item => item.productName && !isNaN(item.price));
+
+    let transaction
+    try {
+      // 开启事务
+      const transaction = await sequelize.transaction();
+
+      // 查找现有餐厅资料
+      const restaurant = await Restaurant.findByPk(restId, {
+        include: [Meal],
+        transaction
+      });
+
+      if (!restaurant) {
+        throw new Error('Restaurant not found');
+      }
+
+      // 更新餐厅资料
+      await restaurant.update({ name, tel, address }, { transaction });
+
+      // 遍历并更新或创建餐点数据
+      for (const item of descriptionArray) {
+        const [meal, created] = await Meal.findOrCreate({
+          where: {
+            restaurantId: restaurant.id,
+            meals: item.productName
+          },
+          defaults: { price: item.price },
+          transaction
+        });
+
+        if (!created) {
+          // 如果餐点已经存在，更新价格
+          await meal.update({ price: item.price }, { transaction });
+        }
+      }
+
+      // 如果需要，可以删除不再包含在descriptionArray中的餐点
+      const currentMeals = descriptionArray.map(item => item.productName);
+      await Meal.destroy({
+        where: {
+          restaurantId: restaurant.id,
+          meals: { [Op.notIn]: currentMeals }
+        },
+        transaction
+      });
+
+      // 提交事务
+      await transaction.commit();
+
+      res.redirect(`/admin/restaurants/${restId}`);
     } catch (err) {
       // 出现错误时回滚事务
       if (transaction) await transaction.rollback();
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,8 +6,12 @@ const { authenticatedAdmin } = require('../../middlewares/auth')
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', authenticatedAdmin, adminController.createRestaurants)
+router.get('/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
+router.get('/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
 router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
+router.put('/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
 router.post('/restaurants', authenticatedAdmin, adminController.psotRestaurants)
+
 
 router.get('/allorders', authenticatedAdmin, adminController.getAllOrders)
 router.get('/dailyorder/:date', authenticatedAdmin, adminController.getDailyOrder)
